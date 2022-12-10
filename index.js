2a5e42f980209f6e6f6251e7b93ba155075346b6
Rubem Corrêa <111711106+Rxxbm@users.noreply.github.com>
2022-12-10 18:40:57 -0300

    Arrumando o Diretório

--- index.js
+++ index.js
@@ -1,51 +1,72 @@
 function adicionar(){
     const txt1 = (document.querySelector("#txt").value)
-    const br = document.createElement('br')
+    const h4 = document.createElement('h4')
     const txt2 = (document.querySelector("#txt2").value)
     const concluir = document.createElement('button')
     const editar = document.createElement('button')
+    const resultado = document.createElement("div")
+    const divTxts = document.createElement('div')
+    const divBtn = document.createElement('div')
+    resultado.className = "resultado"
+    resultado.style.display = "flex"
+    resultado.style.justifyContent = "space-around"
     concluir.innerHTML= "Concluída"
     editar.innerHTML = "Editar"
-    const novaLi = document.createElement('h4');
+    const novaLi = document.createElement('p');
     const excluir = document.createElement('button');
     excluir.innerHTML= "Remover Tarefa";
-    novaLi.innerHTML = "TAREFA: "+ txt1 + "<br> DETALHAMENTO: " + txt2;
+    novaLi.innerHTML = " <strong>Tarefa:</strong> "+ txt1 + "<br> <strong>Detalhamento:</strong> " + txt2;
     concluir.addEventListener('click', function() {
-        if (novaLi.style.textDecoration == "line-through"){
-            novaLi.style.textDecoration = "none";
+        if (resultado.style.backgroundColor == "green"){
+            resultado.style.backgroundColor = "white";
+            
         }else{
-        novaLi.style.textDecoration = "line-through";
+        resultado.style.backgroundColor = "green";
+        resultado.style.color = "white";
+        resultado.style.justifyContent = "center";
+        divTxts.appendChild(h4);
+        
+        h4.innerText = "Tarefa Concluída!"
+        novaLi.remove()
+        excluir.remove()
+        editar.remove()
+        concluir.remove()
     }}) 
     excluir.addEventListener('click', function(){
         novaLi.remove()
         excluir.remove()
-        br.remove()
         editar.remove()
         concluir.remove()
     })
     editar.addEventListener('click', function(){
         const ed = window.prompt('Entre com o Texto que deseja inserir')
         const eddet = window.prompt("Entre com o detalhamento")
-        novaLi.innerHTML = "TAREFA: "+ ed + "<br> DETALHAMENTO: " + eddet;
+        novaLi.innerHTML = " <strong>Tarefa:</strong> "+ ed + "<br> <strong>Detalhamento:</strong> " + eddet;
     })
-    document.getElementById("resultado").appendChild(novaLi);
-    document.getElementById("resultado").appendChild(excluir);
-    document.getElementById("resultado").appendChild(concluir);
-    document.getElementById("resultado").appendChild(editar);
-    document.getElementById('resultado').appendChild(br)
+    divTxts.appendChild(novaLi);
+    divBtn.appendChild(excluir)
+    divBtn.appendChild(concluir)
+    divBtn.appendChild(editar)
+    resultado.appendChild(divTxts);
+    resultado.appendChild(divBtn)
+    //resultado.appendChild(br)
     
+    document.getElementById("campo").appendChild(resultado);
     document.querySelector("#txt").value = ""
     document.querySelector("#txt2").value = ""
 }
 function exibir(){
     document.getElementById("cadastrar").style.display = "none";
     document.getElementById("exibir").style.display = "block";
+    document.getElementById("dados").style.display = "none";
 }
 function cadastrar(){
     document.getElementById("cadastrar").style.display = "block";
     document.getElementById("exibir").style.display = "none";
+    document.getElementById("dados").style.display = "none";
 }
-
-function login(email, senha){
-    
+function exibirdados(){
+    document.getElementById("cadastrar").style.display = "none";
+    document.getElementById("exibir").style.display = "none";
+    document.getElementById("dados").style.display = "block";
 }
